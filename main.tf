a3c0846fd8be3c335e3641c0e243caeaa77030c6
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-04 12:58:48 -0300

    chore: updated dependency sources

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+locals {
+  extra_disks = {for disk in flatten([
+    for host_index in range(var.host_count) : [
+      for k, v in var.extra_disks : {
+        basename = k
+        fullname = "${var.rg.name}-${var.name}${format("%04.0f", host_index + 1)}-${k}"
+        host_index = host_index
+        storage_account_type = v["storage_account_type"]
+        disk_size_gb = v["disk_size_gb"]
+      }
+    ]
+  ]) : disk.fullname => disk}
+}
+
 resource "random_password" "this" {
   length  = 16
   special = true
